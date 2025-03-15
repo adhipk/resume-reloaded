abc863b24ab1359aa4bf8450534e10faf659bd67
adhipk <adhip.kashyap@gmail.com>
2025-03-15 15:58:58 -0700

    fix

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 HTML='--standalone --embed-resources --css screen.css'
 PDF='--standalone --embed-resources --css style.css'
 ENG='--pdf-engine=weasyprint'
-mkdir -p "${OUTPUT_DIR:-out}
+mkdir -p "${OUTPUT_DIR:-out}"
 
 pandoc $HTML $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-out}/index.html"
 pandoc $PDF $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-out}/$1.pdf"
