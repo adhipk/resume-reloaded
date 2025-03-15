5cddb0e74549fb1c0fe04a3bab35d280897e4cb3
adhipk <adhip.kashyap@gmail.com>
2025-03-15 15:52:16 -0700

    changes

--- build.sh
+++ build.sh
@@ -5,11 +5,6 @@ HTML='--standalone --embed-resources --css screen.css'
 PDF='--standalone --embed-resources --css style.css'
 ENG='--pdf-engine=weasyprint'
 
-if [ "$2" = "html" ]; then
-    pandoc $HTML $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-output}/index.html"
-elif [ "$2" = "pdf" ]; then
-    pandoc $PDF $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-output}/$1.pdf"
-else
-    echo "Usage: $0 <input-file> <html|pdf>"
-    exit 1
-fi
+
+pandoc $HTML $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-out}/index.html"
+pandoc $PDF $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-out}/$1.pdf"
