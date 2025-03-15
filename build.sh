b78c36b03a37ae10e5fe7e57144535de2394e046
adhipk <adhip.kashyap@gmail.com>
2025-03-15 01:31:19 -0700

    made index html file

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-OUTPUT_DIRECTORY=out
+set -a && source .env && set +a
 
 HTML='--standalone --embed-resources  --css screen.css'
 PDF='--standalone --embed-resources  --css style.css'
 ENG='--pdf-engine=weasyprint'
 
-mkdir -p $OUTPUT_DIRECTORY
-pandoc $PDF  $ENG  -t html -f markdown+smart $1.md -o $OUTPUT_DIRECTORY/$1.pdf
-pandoc $HTML  $ENG  -t html -f markdown+smart $1.md  -o $OUTPUT_DIRECTORY/$1.html
+# pandoc $PDF  $ENG  -t html -f markdown+smart $1.md -o $OUTPUT_DIR/$1.pdf
+pandoc $HTML  $ENG  -t html -f markdown+smart $1.md  -o index.html
