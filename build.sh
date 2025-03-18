eec831b7fe99f4d2488e2e4a2c6bf42ff8ce095d
adhipk <adhip.kashyap@gmail.com>
2025-03-18 13:54:54 -0700

    added ml version of resume

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ HTML='--standalone --embed-resources --css screen.css'
 PDF='--standalone --embed-resources --css style.css'
 ENG='--pdf-engine=weasyprint'
 mkdir -p "${OUTPUT_DIR:-out}"
-
-pandoc $HTML $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-out}/index.html"
+OUT="${2:-index}"
+pandoc $HTML $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-out}/$OUT.html"
 pandoc $PDF $ENG -t html -f markdown+smart $1.md -o "${OUTPUT_DIR:-out}/$1.pdf"
